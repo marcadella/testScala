4f4924e0de49f87bf6dab0f009e310bdca33da2c
Mayeul <mma227@uit.no>
2018-08-12 13:02:17 +0200

    Add Heartbeat classes

--- src/main/scala/mayeul/utils/threads/TimestampChecker.scala
+++ src/main/scala/mayeul/utils/threads/TimestampChecker.scala
@@ -27,22 +27,3 @@ class TimestampChecker(
         }
       }
     )
-
-/**
-  * Periodically checks a `timestamp` and executes once uponTimeoutDo() as soon as the timestamp timeouts.
-  * The timestamp must come from a call to System.currentTimeMillis and its value is ignored when 0
-  * To terminate within `todo` (when it is not related to a halt()):
-  *   throw Terminate
-  */
-class HeartbeatChecker(
-    period: Duration,
-    timeout: Duration,
-    lastHeartbeat: => Long,
-    uponTimeoutDo: => Unit
-) extends TimestampChecker(
-      period,
-      timeout,
-      lastHeartbeat,
-      overTimeoutDo = uponTimeoutDo,
-      autoHalt = true
-    )
