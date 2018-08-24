59ec36e342e0090aa449d9ae525c7367b45b6fa0
Mayeul <mma227@uit.no>
2018-08-24 11:42:11 +0200

    - Add RegexTest

---
+++ src/test/scala/mayeul/patterns/RegexTest.scala
@@ -0,0 +1,31 @@
+package mayeul.patterns
+
+import org.scalatest.{FunSpec, Matchers}
+
+/**
+  * Cf https://regex101.com/
+  * (...) -> capture that can be pattern matched
+  * [...] -> range
+  * ? makes the preceding lazy (without it is greedy)
+  */
+class RegexTest extends FunSpec with Matchers {
+  describe(classOf[RegexTest].getName) {
+    val regex = "^(.+?)[\\W_]?(r1|r2)\\.fastq$".r
+    it("should not match") {
+      val notMatch = "r3.fastq"
+      notMatch.matches(regex.toString) should be(false)
+    }
+    it("should match") {
+      val shouldMatch = "bla_r1.fastq"
+      shouldMatch.matches(regex.toString) should be(true)
+    }
+    it("should extract the captures") {
+      val shouldMatch = "bla_r1.fastq"
+      shouldMatch match {
+        case regex(fileNamePrefix, fileNameSuffix) =>
+          fileNamePrefix should be("bla")
+          fileNameSuffix should be("r1")
+      }
+    }
+  }
+}
