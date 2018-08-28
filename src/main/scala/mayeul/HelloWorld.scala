60ca45c515b3fae94fdfaaf338bfea4bce601656
Mayeul <mma227@uit.no>
2018-08-28 09:26:22 +0200

    Recover accidental commit of HelloWorld

--- src/main/scala/mayeul/HelloWorld.scala
+++ src/main/scala/mayeul/HelloWorld.scala
@@ -1,49 +1,7 @@
 package mayeul
 
-import scala.concurrent._
-import scala.concurrent.duration._
-import scala.util.{Failure, Success}
-import java.util.Date
-
-import org.json4s.NoTypeHints
-import org.json4s.jackson.Serialization
-
 object HelloWorld {
-  implicit val ec = ExecutionContext.global
-
   def main(args: Array[String]): Unit = {
-    val obj = new B
-    println(obj.a)
-    println(obj.aa)
-    println(obj.b)
-    println(obj.bb)
-    println(obj.c)
-    println(obj.cc)
-    println(obj.d)
-    println(obj.dd)
+    println("Hello world!")
   }
-}
-
-trait A {
-  val a: Int = 6
-  val aa = a * a
-
-  def b: Int = 7
-  val bb = b * b
-
-  def c: Int = 8
-  val cc = c * c
-
-  def d: Int = 9
-  lazy val dd = d * d
-}
-
-class B extends A {
-  override val a = 2
-
-  override val b = 3
-
-  override lazy val c = 4
-
-  override val d = 5
 }
