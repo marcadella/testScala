c09b6f13af40cb14c8adbf4c984e0d142ab552d5
Mayeul <mma227@uit.no>
2018-12-05 17:40:51 +0100

    Improves LazyInitialization

--- src/test/scala/mayeul/language/initialization/LazyInitializationTest.scala
+++ src/test/scala/mayeul/language/initialization/LazyInitializationTest.scala
@@ -3,61 +3,40 @@ package mayeul.language.initialization
 import org.scalatest.{FunSpec, Matchers}
 
 /**
-  * An overridden val is properly resolved, BUT
-  * another val using it would get 0 or null.
-  * -> preferred method -> d
+  * A val in a subclass is null/0 when used in the parent class
+  * The rules of thumb are:
+  *   - never have val on either side of an override unless the subclass defines the val in its constructor
+  *   - since def can be overridden by val it is still possible to make the mistake, so just go for lazy val all the time!
+  *   - Note that it is not enough to have a lazy val in the subclass: it must not be defined with any val inside!
   */
-trait A {
-  val a: Int
-  val aa = a * a
-
-  def b: Int
-  val bb = b * b
-
-  def c: Int //Would work with val
-  val cc = c * c
-
-  def d: Int //Would work with val
-  lazy val dd = d * d
-
-  val e: Int = 10
-  lazy val ee = e * e
+abstract class A {
+  def a: Int = 1
+  lazy val b: Int = 2
+  val aCheck = a
+  val bCheck = b //ok since b and bb are lazy val
+  val c: Int //Ok since passed by parameter from B
 }
 
-class B extends A {
-  val a = 2
-
-  val b = 3
-
-  lazy val c = 4
-
-  val d = 5
-
-  override val e = 6
+class B(val c: Int) extends A {
+  val aa = 10 //val = 0 in A
+  lazy val bb = 20
+  override lazy val a = aa //called in A so a takes value 0
+  override lazy val b = bb
 }
 
 class LazyInitializationTest extends FunSpec with Matchers {
-  val obj = new B
+  val obj = new B(7)
   describe(classOf[LazyInitializationTest].getName) {
-    it("should have wrong value for aa") {
-      obj.a should be(2)
-      obj.aa should be(0)
+    it("should have wrong value for a and aCheck") {
+      obj.a should be(0)
+      obj.aCheck should be(0)
     }
-    it("should have wrong value for bb") {
-      obj.b should be(3)
-      obj.bb should be(0)
+    it("should have correct value for b and bCheck") {
+      obj.b should be(20)
+      obj.bCheck should be(20)
     }
-    it("should have right value for cc") {
-      obj.c should be(4)
-      obj.cc should be(16)
-    }
-    it("should have right value for dd") {
-      obj.d should be(5)
-      obj.dd should be(25)
-    }
-    it("should have right value for ee") {
-      obj.e should be(6)
-      obj.ee should be(36)
+    it("should have right value for c") {
+      obj.c should be(7)
     }
   }
 }
